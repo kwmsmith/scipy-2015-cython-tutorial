9cb2938f4ad30f76ca636839d43c61075f7e5832
Kurt Smith <ksmith@enthought.com>
2015-07-06 07:31:09 -0500

    Tweaks.

--- exercises/03-wrapping/01-levenshtein/test_levenshtein.py
+++ exercises/03-wrapping/01-levenshtein/test_levenshtein.py
@@ -1,23 +1,12 @@
 from timeit import timeit
 
-import levenshtein_cython
-import levenshtein_solution
-
-def test(func):
-    assert func("kitten", "sitting") == 3
-    assert func("sitting", "kitten") == 3
-    assert func("", "asdf") == 4
-    assert func("jkl;", "") == 4
-    assert func("", "") == 0
-    assert func("qwerty", "qwerty") == 0
-
-# test(levenshtein_cython.levenshtein_distance)
-test(levenshtein_solution.levenshtein_distance)
-
 N = 200000
 
 print "*" * 80
-print "wrapped levenshtein(): %.2f" % timeit("levenshtein_distance('kitten', 'sitting')",
+# print "wrapped levenshtein(): %.2f" % timeit("levenshtein_distance('kitten', 'sitting')",
+                                        # "from levenshtein_cython import levenshtein_distance",
+                                        # number=N)
+print "solution levenshtein(): %.2f" % timeit("levenshtein_distance('kitten', 'sitting')",
                                         "from levenshtein_solution import levenshtein_distance",
                                         number=N)
 print "*" * 80
