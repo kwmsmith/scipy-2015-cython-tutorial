ddbc17417f7a5915fa87c5c7f78fa588ea8333bb
Thomas A Caswell <tcaswell@bnl.gov>
2015-07-06 10:15:57 -0400

    MNT: update test_hamming
     - use print function
     - pass in bytes to optimal solution

--- exercises/02-typing/01-hamming-distance/test_hamming.py
+++ exercises/02-typing/01-hamming-distance/test_hamming.py
@@ -1,17 +1,18 @@
+from __future__ import print_function
 from timeit import timeit
 
-print "*" * 80
-print "python hamming_sum(): %.2f" % timeit('hamming_sum("qwertyuio", "asdfghjkl")',
-                                            'from hamming import hamming_sum')
-print "cython hamming_sum(): %.2f" % timeit('hamming_sum("qwertyuio", "asdfghjkl")',
-                                            'from hamming_cython import hamming_sum')
-print "optimal hamming_sum(): %.2f" % timeit('hamming_sum("qwertyuio", "asdfghjkl")',
-                                            'from hamming_cython_solution import hamming_sum')
-print "-" * 80
-print "python hamming_loop(): %.2f" % timeit('hamming_loop("qwertyuio", "asdfghjkl")',
-                                             'from hamming import hamming_loop')
-print "cython hamming_loop(): %.2f" % timeit('hamming_loop("qwertyuio", "asdfghjkl")',
-                                             'from hamming_cython import hamming_loop')
-print "optimal hamming_loop(): %.2f" % timeit('hamming_loop("qwertyuio", "asdfghjkl")',
-                                             'from hamming_cython_solution import hamming_loop')
-print "*" * 80
+print("*" * 80)
+print("python hamming_sum(): %.2f" % timeit('hamming_sum("qwertyuio", "asdfghjkl")',
+                                            'from hamming import hamming_sum'))
+print("cython hamming_sum(): %.2f" % timeit('hamming_sum("qwertyuio", "asdfghjkl")',
+                                            'from hamming_cython import hamming_sum'))
+print("optimal hamming_sum(): %.2f" % timeit('hamming_sum("qwertyuio", "asdfghjkl")',
+                                            'from hamming_cython_solution import hamming_sum'))
+print("-" * 80)
+print("python hamming_loop(): %.2f" % timeit('hamming_loop("qwertyuio", "asdfghjkl")',
+                                             'from hamming import hamming_loop'))
+print("cython hamming_loop(): %.2f" % timeit('hamming_loop("qwertyuio", "asdfghjkl")',
+                                             'from hamming_cython import hamming_loop'))
+print("optimal hamming_loop(): %.2f" % timeit('hamming_loop(b"qwertyuio", b"asdfghjkl")',
+                                             'from hamming_cython_solution import hamming_loop'))
+print("*" * 80)
